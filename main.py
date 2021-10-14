63efe3454aba7cab6d17a79abbd50447d5f4218b
GunalanD95 <Guna19may2015@gmail.com>
2021-10-14 09:43:44 +0530

    fix

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 import  models
 from database import engine 
 from fastapi.templating import Jinja2Templates
+
+import sys
+sys.path.append(r"C:\Users\admin\Desktop\FASTAPI-TUT\fastapi-env\ecommerce")
+
 from ecom.routers import product
+
+
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
-app.include_router(product.router) 
+# app.include_router(product.router) 
 
 templates = Jinja2Templates(directory="templates")
 
